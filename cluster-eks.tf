3ebe49c4c11eae713be8a2bffce8afa94cbdb7aa
ySodias <63486008+ySodias@users.noreply.github.com>
2024-03-09 23:46:23 -0300

    change security group

--- cluster-eks.tf
+++ cluster-eks.tf
@@ -168,7 +168,7 @@ resource "aws_eks_node_group" "node-grp" {
 
   remote_access {
     ec2_ssh_key               = var.ami_key_pair_name
-    source_security_group_ids = aws_security_group.allow_tls.id
+    source_security_group_ids = [aws_security_group.allow_tls.id]
   }
 
   scaling_config {
