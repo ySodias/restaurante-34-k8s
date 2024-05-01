99a9c33a69f7e8b5bef53dfb9d2de58ec6b5936c
ySodias <63486008+ySodias@users.noreply.github.com>
2024-05-01 10:38:22 -0300

    Update ec2-security-group.tf

--- ec2-security-group.tf
+++ ec2-security-group.tf
@@ -8,16 +8,16 @@ resource "aws_security_group" "allow_ssh" {
       "0.0.0.0/0"
     ]
     from_port = 22
-        to_port = 22
-        protocol = "tcp"
+    to_port   = 22
+    protocol  = "tcp"
   }
 
   egress {
-   from_port = 0
-   to_port = 0
-   protocol = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "allow_ssh"
@@ -34,16 +34,16 @@ resource "aws_security_group" "allow_sonar" {
       "0.0.0.0/0"
     ]
     from_port = 9000
-        to_port = 9000
-        protocol = "tcp"
+    to_port   = 9000
+    protocol  = "tcp"
   }
 
   egress {
-   from_port = 0
-   to_port = 0
-   protocol = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
- }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "allow_ssh"
